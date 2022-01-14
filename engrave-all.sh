50bd0810cab9e29c1bf8ff08310ee7b721a0d391
Johannes Benoit <jbenoit@t-online.de>
2022-01-14 01:31:13 +0100

    productionscript

--- engrave-all.sh
+++ engrave-all.sh
@@ -12,23 +12,25 @@ strindex() {
   [[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
 }
 
-for path in $(ls -d1 */*.midi)
+for path in $(ls -d1 */*.ly)
 do
     pos=$(strindex $path "/")
     folder=${path:0:pos} 
     file=${path:pos+1} 
     pos=$(strindex $file ".")
     rawfile=${file:0:pos}
+    ly=$folder/$rawfile.ly
     wav=/tmp/midi/$rawfile.wav
     mp3=out/$rawfile.mp3
     pdfin=$folder/$rawfile.pdf
     pdfout=out/$rawfile.pdf
-    echo "...   $path --> $wav --> $mp3" 
+    echo "...   $ly -- $wav -- $mp3  -- $pdfout" 
 
+    lilypond -dno-point-and-click -ddelete-intermediate-files --pdf -o$folder/$rawfile $ly
 
+    cp $pdfin $pdfout
     timidity $path -OwM -o $wav
     lame -hb128 $wav $mp3
-    gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=$pdfout $pdfin
     
 #echo "[$(tput setaf 2)ok$(tput sgr 0)] processed files:  $(ls out/* | wc -w)"
  
